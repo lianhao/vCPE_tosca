6572a26db52c82a2fbc7e3d18b173fe814c5f064
Lianhao Lu <lianhao.lu@intel.com>
2018-04-12 16:17:16 +0800

    Update csar generation script to include fake artifact file

--- generate_csar.sh
+++ generate_csar.sh
@@ -8,7 +8,7 @@ DIRS="infra vbrgemu vbng vgmux vgw"
 
 for dir in $DIRS; do
     cd $ROOT_DIR/$dir
-    zip -r $ROOT_DIR/$dir/$dir.csar artifacts/ TOSCA-Metadata/ onap_dm.yaml $PREFIX$dir.yaml
+    zip -r $ROOT_DIR/$dir/$dir.csar artifacts/ TOSCA-Metadata/ onap_dm.yaml $PREFIX$dir.yaml ubuntu_16.04
     rm -f $DEST/$dir.csar
     mv $ROOT_DIR/$dir/$dir.csar $DEST/
 done
